cc4082d7c9b32495f45441357391eb8b0c5a37ad
BryanIvander <bryanivander22@gmail.com>
2024-03-18 10:56:15 +0700

    week 4 initials

---
+++ app/src/main/java/com/example/advweek4160420137/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.advweek4160420137
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
